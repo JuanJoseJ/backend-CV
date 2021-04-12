340e789386e9e94f91d0f6bd94e40c8428846612
JuanJoseJ <jjj.1998@hotmail.com>
2021-04-11 22:28:22 -0500

    Revert "Merge remote-tracking branch 'origin/master'"
    This reverts commit 88241c09ffbb1d8255aad1acfe82b583d2bd6659, reversing
    changes made to df7a0f3b9478f17a7d26c9f9e94fcf0f22a6ee5f.

--- config/env/production/server.js
+++
@@ -1,9 +0,0 @@
-module.exports = ({ env }) => ({
-  host: env('HOST', '192.241.143.74'),
-  port: env.int('PORT', 8001),
-  admin: {
-    auth: {
-      secret: env('ADMIN_JWT_SECRET', '93879c9d889ba0fcd1ebeaab14d5cd33'),
-    },
-  },
-});
